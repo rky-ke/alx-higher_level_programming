78238391e7b761a83af423b1df719ff51dab8c87
rky-ke <ronaldyegon15@gmail>
2023-01-12 13:18:29 +0000

    a function that adds two integers and returns the result

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -2,8 +2,4 @@
 # Author - Ronald Yegon
 def add(a, b):
     """Return the addition of a and b."""
-    return (a + b)#!/usr/bin/python3
-# Author - Bamidele Adefolaju
-def add(a, b):
-    """Return the addition of a and b."""
     return (a + b)
