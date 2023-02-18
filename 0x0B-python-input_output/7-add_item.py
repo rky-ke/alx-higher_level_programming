c0bf4cf1e84ef94e4e1b654f9c6d38c66440d07e
rky-ke <ronaldyegon15@gmail>
2023-02-18 04:45:23 +0000

    script that adds all arguments to a Python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,7 +5,6 @@ import sys
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
     args= sys.argv
     filename= "add_item.json"
     with open(filename, 'a+', encoding = "utf-8") as f:
@@ -13,4 +12,3 @@ if __name__ == "__main__":
         arg_list.extend(args[1:])
         save_to_json_file(arg_list, filename)
         load_from_json_file(filename)
-
