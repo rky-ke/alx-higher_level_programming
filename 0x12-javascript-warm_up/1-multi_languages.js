f1bb78e68f59ca8a754c3562ad5111b4a0c6cd0e
rky-ke <ronaldyegon15@gmail.com>
2023-06-12 16:43:32 +0300

    3 Languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//script that prints 3 lines
+let langComment = "C is fun";
+console.log(langComment);
+langComment = "Python is cool";
+console.log(langComment);
+langComment = "Javascript is amazing";
