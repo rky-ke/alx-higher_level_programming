825e0bd0cf92801538390d96a3ff8b5861c8b4e4
rky-ke <ronaldyegon15@gmail.com>
2023-06-25 12:09:33 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const command = process.argv.length;
-if (command === 2) {
-  console.log('No Argument');
-} else if (command === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+// const command = process.argv.length;
+// if (command === 2) {
+//   console.log('No Argument');
+// } else if (command === 3) {
+//   console.log('Argument found');
+// } else {
+//   console.log('Arguments found');
+// }
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
