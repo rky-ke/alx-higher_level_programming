8660ee6603911a1fb96c81d68d1eda22065e2c92
rky-ke <ronaldyegon15@gmail.com>
2023-06-25 06:43:10 +0000

    multi languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-let langComment = 'C is fun';
-console.log(langComment);
-langComment = 'Python is cool';
-console.log(langComment);
-langComment = 'Javascript is amazing';
-console.log(langComment);
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
+
