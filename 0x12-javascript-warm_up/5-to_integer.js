ee528bb6cad0205028fd2d33eb6d2cd656f8ed3f
rky-ke <ronaldyegon15@gmail.com>
2023-06-26 13:51:57 +0000

    Converting arguments into integers

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 const number = Math.floor(Number(process.argv[2]));
-if (Number.isInteger(number)) {
-  console.log("My number: ", number);
-} else {
-  console.log("Not a number");
-}
+console.log(isNaN(number) ? 'Not a number' : `My number: ${number}`);
