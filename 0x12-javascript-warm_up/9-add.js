cf98d935399b743ee0d46007f311af2708c9f677
rky-ke <ronaldyegon15@gmail.com>
2023-06-30 19:13:49 +0000

    Add arguments

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,6 +4,11 @@ const a = process.argv[2];
 const b = process.argv[3];
 
 function add(a,b) {
-  const sum = Number(a) + Number(b);
+  const sum = parseInt(a) + parseInt(b);
   console.log(sum);
+}
+if (a !== undefined && b !== undefined) {
+  add(a, b);
+} else {
+  console.log('NaN');
 }
