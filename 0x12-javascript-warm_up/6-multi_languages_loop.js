51e6f4c1a01823776000a823af2ebca84db1c320
rky-ke <ronaldyegon15@gmail.com>
2023-06-30 18:18:41 +0000

    multilanguage

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const liness = ["C is fun", "Python is cool", "Javascript is amazing"];
+const lines = ["C is fun", "Python is cool", "Javascript is amazing"];
 
 for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
