a0655decd0d3d1dea155ca92cb0c42a48f7f32f3
rky-ke <ronaldyegon15@gmail.com>
2023-06-30 18:25:50 +0000

    multilanguage

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 const lines = ["C is fun", "Python is cool", "Javascript is amazing"];
 
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
-}
+lines.forEach(line => console.log(line));
