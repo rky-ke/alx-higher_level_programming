db08cae6544cf5e488ab644f3f24ed1627840b6e
rky-ke <ronaldyegon15@gmail.com>
2023-07-16 15:09:39 +0000

    multilnguages in an array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 const multilanguages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < multilanguages.length; i++) {
-  console.log(multilanguages[i]);i
+  console.log(multilanguages[i]);
 }
