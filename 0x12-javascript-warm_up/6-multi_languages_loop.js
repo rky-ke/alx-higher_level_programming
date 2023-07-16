3417a742ee040039ce83ef3ce1d6bc68547d860c
rky-ke <ronaldyegon15@gmail.com>
2023-07-16 14:58:06 +0000

    multilnguages i an array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-# script that prints 3 lines by using an array
-const lines = ["C is fun", "Python is cool", "Javascript is amazing"];
-
-lines.forEach(line => console.log(line));
+const multilanguages = ["C is fun", "Python is cool", "Javascript is amazing"];
+for (let i = 0; i < multilanguages.length; i++) {
+  console.log(multilanguages[i]);
+}
