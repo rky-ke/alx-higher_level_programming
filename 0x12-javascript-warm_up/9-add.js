c6ded4022df54c1937385a8061904e5b589b9763
rky-ke <ronaldyegon15@gmail.com>
2023-07-16 15:48:32 +0000

    9AddJs

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const a = process.argv[2];
 const b = process.argv[3];
 
@@ -8,7 +7,7 @@ function add(a,b) {
   console.log(sum);
 }
 if (a !== undefined && b !== undefined) {
-  add(a, b);
+  add(a , b);
 } else {
   console.log('NaN');
 }
