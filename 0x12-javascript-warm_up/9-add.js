f74c318d53adedf70c6536fc7c53ef01c6a5ba13
rky-ke <ronaldyegon15@gmail.com>
2023-07-23 15:21:34 +0300

    9-Add.js update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
-function add (a, b) { // Add space between 'add' and '('
+function add (a, b) {
   return a + b;
 }
 const x = parseInt(process.argv[2]);
 const y = parseInt(process.argv[3]);
 
-console.log(add (x, y));
+console.log(add(x, y));
 
